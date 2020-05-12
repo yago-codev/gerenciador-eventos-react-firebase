7c9ac76e85986ab9c77f70007f429eda043f3f9c
yago-codev <yagomilano92@gmail.com>
2020-05-11 22:33:01 -0300

    Implementando a feature de criação de eventos e persistindo os dados no Firebase

--- src/view/CreateEvent/index.js
+++ src/view/CreateEvent/index.js
@@ -9,6 +9,50 @@ import "./styles.css";
 
 export default function CreateEvent() {
   const [msgTipo, setMsgTipo] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [tipo, setTipo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [data, setData] = useState(null);
+  const [hora, setHora] = useState(null);
+  const [imagem, setImagem] = useState(null);
+  const [carregando, setCarregando] = useState(false);
+
+  const storage = firebase.storage(); // storage de armazenamento das imagens de eventos
+  const db = firebase.firestore(); // banco de dados onde serão armazenadas as infos dos eventos
+
+  const usuario = useSelector((state) => state.userEmail);
+
+  function criarEvento() {
+    setMsgTipo(null);
+    setCarregando(true);
+
+    storage
+      .ref(`images/${imagem.name}`)
+      .put(imagem)
+      .then(() => {
+        db.collection("eventos")
+          .add({
+            titulo,
+            tipo,
+            descricao,
+            data,
+            hora,
+            usuario: usuario,
+            visualizacoes: 0,
+            imagem: imagem.name,
+            publico: 1,
+            criacao: new Date(),
+          })
+          .then(() => {
+            setMsgTipo("sucesso");
+            setCarregando(false);
+          })
+          .catch((err) => {
+            setMsgTipo("erro");
+            setCarregando(false);
+          });
+      });
+  }
 
   return (
     <>
@@ -24,13 +68,17 @@ export default function CreateEvent() {
           <div className="form-group">
             <input
               type="text"
-              className="form-control"
+              className="form-control text-white"
               placeholder="Nome do Evento"
+              onChange={(e) => setTitulo(e.target.value)}
             />
           </div>
 
           <div className="form-group">
-            <select className="form-control">
+            <select
+              className="form-control"
+              onChange={(e) => setTipo(e.target.value)}
+            >
               <option disabled selected value>
                 Selecione um tipo...
               </option>
@@ -42,30 +90,61 @@ export default function CreateEvent() {
           </div>
 
           <div className="form-group">
-            <textarea className="form-control" rows="7" />
+            <textarea
+              className="form-control"
+              rows="7"
+              placeholder="Descrição do evento..."
+              onChange={(e) => setDescricao(e.target.value)}
+            />
           </div>
 
           <div className="form-group">
             <div className="row">
               <div className="col-6">
                 <label className="text-white">Data:</label>
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  onChange={(e) => setData(e.target.value)}
+                />
               </div>
 
               <div className="col-6">
                 <label className="text-white">Hora:</label>
-                <input type="time" className="form-control" />
+                <input
+                  type="time"
+                  className="form-control"
+                  onChange={(e) => setHora(e.target.value)}
+                />
               </div>
             </div>
           </div>
 
           <div className="form-group">
-            <input type="file" className="form-control" />
+            <input
+              type="file"
+              className="form-control"
+              onChange={(e) => setImagem(e.target.files[0])}
+            />
           </div>
 
-          <button type="button" className="btn-cadastro btn btn-lg btn-block">
-            Criar Evento
-          </button>
+          {carregando === true ? (
+            <div className="text-center">
+              <div className="spinner-border text-white" role="status">
+                <span className="sr-only">Carregando...</span>
+              </div>
+            </div>
+          ) : (
+            <>
+              <button
+                type="button"
+                className="btn btn-lg btn-block mt-3 mb-3 btn-cadastro"
+                onClick={criarEvento}
+              >
+                Cadastrar
+              </button>
+            </>
+          )}
         </form>
 
         <div className="msg-login text-white text-center mt-2">
