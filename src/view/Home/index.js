52e8e0e1c8d82810bc0e54bc31fe6dc403558d62
yago-codev <yagomilano92@gmail.com>
2020-05-13 05:34:08 -0300

    Dinamizando os cards de eventos

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import Navbar from "../../components/Navbar";
 import EventCard from "../../components/EventCard";
+import firebase from "../../config/firebase";
 
 import "./styles.css";
 
 export default function Home() {
+  const [eventos, setEventos] = useState([]);
+
+  let listaEventos = [];
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("eventos")
+      .get()
+      .then(async (result) => {
+        await result.docs.map((doc) => {
+          return listaEventos.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+
+        setEventos(listaEventos);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
-      <EventCard />
+      <div className="home">
+        <div className="container">
+          <div className="row">
+            {eventos.map((evento) => (
+              <EventCard
+                key={evento.id}
+                id={evento.id}
+                img={evento.imagem}
+                titulo={evento.titulo}
+                descricao={evento.descricao}
+                visualizacoes={evento.visualizacoes}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
